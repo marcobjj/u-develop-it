3cb6fc4141c9be2c0ef0a390f4d75e569ec28170
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-10-07 17:19:26 -0700

    adds modules and voter routes

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/database');
+
+router.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+ router.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+ module.exports = router;
